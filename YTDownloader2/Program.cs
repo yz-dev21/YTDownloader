ed3eba1dcebfe427ddd46f731f5753f791b6e880
Bugger <yunjae00734@gmail.com>
2022-07-13 23:59:13 +0900

    2.0 UPDATE (WIP)

---
+++ YTDownloader2/Program.cs
@@ -0,0 +1,12 @@
+namespace YTDownloader2
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
